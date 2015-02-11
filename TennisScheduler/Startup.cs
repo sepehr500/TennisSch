9b60ec482a785c690b035de1a70a9a4ef34de1f2
sepehr500 <sepehr411@gmail.com>
2015-02-11 15:32:53 -0500

    First Commit

---
+++ TennisScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TennisScheduler.Startup))]
+namespace TennisScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
